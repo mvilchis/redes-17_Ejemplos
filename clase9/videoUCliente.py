da51f5550dfdce8ac03d9afae96aae69098257d5
Miguel Vilchis <mvilchis@ciencias.unam.mx>
2016-10-29 11:23:19 -0500

    Se corrigen errores en video udp

--- clase9/videoUCliente.py
+++ clase9/videoUCliente.py
@@ -21,33 +21,57 @@ manda video
 """
 import socket
 import cv2
+import cv2.cv as cv
+
 import pickle
 import sys
 import struct
+import base64
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4 import QtGui,QtCore
 
 ##Uso de https://docs.python.org/2/library/struct.html
+
 """ Constantes de tpc """
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 
 my_socket = ''
+def get_cap_size(capture):
+    img = cv.QueryFrame(capture)
 
+    jpgstring = cv.EncodeImage(".jpeg", img).tostring()
+    jpglen = len(jpgstring)
+    split = jpglen/BUFFER_SIZE
+    return split
 def main ():
     global my_socket
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,socket.IPPROTO_UDP)#socket.SOCK_STREAM)
     my_socket.connect((TCP_IP, TCP_PORT))
-    cap = cv2.VideoCapture(0)
+    capture = cv.CaptureFromCAM(0)
 
+
+    address = (TCP_IP,TCP_PORT)
+    split = get_cap_size(capture)
+    splittedstr = [""]*split
+    my_socket.sendto(str(split),address)
     while(True):
-        ret, frame = cap.read()
-        cv2.imshow('Cliente',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        img = cv.QueryFrame(capture)
+
+        jpgstring = cv.EncodeImage(".jpeg", img).tostring()
+        jpglen = len(jpgstring)
+        for i in range(split-1):
+            splittedstr[i] = jpgstring[jpglen/split*i:jpglen/split*(i+1)]
+
+        splittedstr[split-1] = jpgstring[jpglen/split*(split-1):]
+        for i in range(split):
+            my_socket.sendto(splittedstr[i],address)
+        if cv.WaitKey(10) == 27:
             break
-        data = pickle.dumps(frame)
-        package = struct.pack("L", len(data)) + data,
-        for i in range(sys.getsizeof(package)/BUFFER_SIZE):
-            my_socket.sendto(package[i:(i+1)*BUFFER_SIZE],(TCP_IP,TCP_PORT))
+
+
     cap.release()
     cv2.destroyAllWindows()
     my_socket.close()
--- clase9/videoUServidor.py
+++ clase9/videoUServidor.py
@@ -22,6 +22,7 @@ de reproduce el video
 
 import socket
 import cv2
+
 import numpy
 import pickle
 import struct
@@ -36,30 +37,23 @@ def main():
     global my_socket
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,socket.IPPROTO_UDP)#socket.SOCK_STREAM)
     my_socket.bind((TCP_IP, TCP_PORT))
-    #my_socket.listen(10)
-    #conn, addr = my_socket.accept()
-    payload_size = struct.calcsize("L")
     data = ""
+    split = int(my_socket.recvfrom(BUFFER_SIZE)[0])
+    imstr = [""] * split
     while True:
-        #Recibimos el frame completo
-        while len(data) < payload_size:
-            #data += conn.recv(BUFFER_SIZE)
-            data+= my_socket.recvfrom(BUFFER_SIZE)[0]
-        packed_msg_size = data[:payload_size]
+        for i in range(split):
+            imstr[i], addr = my_socket.recvfrom(BUFFER_SIZE * 32)
+        jpgstring = "".join(imstr)
+        narray = numpy.fromstring(jpgstring, dtype="uint8")
+        decimg = cv2.imdecode(narray, 1)
 
-        data = data[payload_size:]
-        msg_size = struct.unpack("L", packed_msg_size)[0]
-
-        while len(data) < msg_size:
-            #data += conn.recv(BUFFER_SIZE)
-            data += my_socket.recvfrom(BUFFER_SIZE)[0]
-
-        frame_data = data[:msg_size]
-        data = data[msg_size:]
-
-        frame=pickle.loads(frame_data)
-        cv2.imshow('Servidor',frame)
-        cv2.waitKey(10)
+        try:
+            cv2.imshow("camara servidor", decimg)
+        except Exception as e:
+            print "show error"
+            continue
+        if  cv2.waitKey(10) == 27:
+            break
 
     cv2.destroyAllWindows()
 
